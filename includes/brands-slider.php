5130ce5045a90af353c1c83ef4ed1056c9450aa4
prince1998 <priyanshramnani21@gmail.com>
2020-04-06 20:17:53 +0530

    Exotic Fleet

---
+++ includes/brands-slider.php
@@ -0,0 +1,56 @@
+<link rel="stylesheet" href="./css/styles.css">
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/cadillac.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/chevrolet.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/isuzu.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/rolls_royce.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/audi.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/bentley.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/honda.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/tesla.png" class = "brand-image" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+
+</div><!-- /.logo-slider -->
